64a778911558f0e7a00e774eb795853195bb433a
Ubereats9481 <allen91220@gmail.com>
2023-03-06 23:34:36 +0800

    fix js
    fix js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var mes = document.querySelector('.mes');
 var p = document.querySelector('#sorry');
 p.style.top = '0';
 p.style.left = '0';
+var sor = ['Sorry', '抱歉', 'ごめん', 'désolé', '죄송합니다', 'Verzeihung', 'Lo siento', 'Scusa', 'क्षमा मांगना'];
 var sor_count = 0;
 
 setInterval(function () {
@@ -16,7 +17,6 @@ setInterval(function () {
 }, 50);
 
 setInterval(function () {
-    var sor = ['Sorry', '抱歉', 'ごめん', 'désolé', '죄송합니다', 'Verzeihung', 'Lo siento', 'Scusa', 'क्षमा मांगना'];
     p.innerHTML = sor[sor_count % 9];
     sor_count++;
 }, 3000);
